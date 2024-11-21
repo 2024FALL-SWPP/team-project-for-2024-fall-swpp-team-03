40e4e9229e83063d9761dc029558acd1279d6638
yeonjae <30648783+yeonjae11@users.noreply.github.com>
2024-11-21 22:10:22 +0900

    Feature/stage lifecycle (#9)
    * add: stage director base
    
    * add: Abstractions for StageDirectorBase
    
    * fix: error correct in SpawnObjects
    
    * chore: redefinition for stageDirector
    
    ---------
    
    Co-authored-by: integraldx <integraldx@integraldx.me>

---
+++ Assets/SWPPT3/Scripts/Main/Utility/Singleton/MonoWeakSingleton.cs
@@ -0,0 +1,57 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace SWPPT3.Main.Utility.Singleton
+{
+    public abstract class MonoWeakSingleton : MonoBehaviour { }
+
+    public abstract class MonoWeakSingleton<T> : MonoWeakSingleton where T : MonoWeakSingleton<T>
+    {
+        private static T s_instance;
+
+        private static readonly object _mutex = new();
+
+        [CanBeNull]
+        public static T Instance
+        {
+            get
+            {
+                if (s_instance != null) return s_instance;
+
+                lock (_mutex)
+                {
+                    var findResult = FindObjectsOfType<T>();
+
+                    switch (findResult.Length)
+                    {
+                        case 0:
+                            Debug.LogWarning($"[{nameof(MonoWeakSingleton)}]: No instance of {nameof(T)} found. Returning null.");
+                            return null;
+
+                        case > 1:
+                            Debug.LogWarning($"[{nameof(MonoWeakSingleton)}]: Multiple instances of {nameof(T)} found. Returning first, ignoring others.");
+                            return s_instance = findResult[0];
+
+                        case 1:
+                            return s_instance = findResult[0];
+
+                        default:
+                            throw new ArgumentOutOfRangeException(
+                                nameof(findResult.Length),
+                                $"{nameof(FindObjectOfType)} returned negative length array");
+                    }
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            lock (_mutex)
+            {
+                s_instance = null;
+            }
+        }
+    }
+}
