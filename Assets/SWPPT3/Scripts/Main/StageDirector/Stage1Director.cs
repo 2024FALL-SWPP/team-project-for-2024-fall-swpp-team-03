130415f3d9644645c67627b6e6d09a6717f0cd40
jihu0331 <70699703+jihu0331@users.noreply.github.com>
2024-11-29 13:15:06 +0900

    add: GameManager

--- Assets/SWPPT3/Scripts/Main/StageDirector/Stage1Director.cs
+++ Assets/SWPPT3/Scripts/Main/StageDirector/Stage1Director.cs
@@ -1,6 +1,9 @@
+using UnityEngine;
+using SWPPT3.Main.Manager;
+
 namespace SWPPT3.Main.StageDirector
 {
-    public class Stage1Director : StageDirectorBase
+    public class Stage1Director : StageManager
     {
         public override void InitializeStage()
         {
