0796e96809080c6e98cff9b1c8809b13e5055271
integraldx <integraldx@integraldx.me>
2024-12-04 19:06:51 +0900

    add: application manager

---
+++ Assets/SWPPT3/Scripts/Main/Manager/ApplicationManager.cs
@@ -0,0 +1,12 @@
+using SWPPT3.Main.Utility.Singleton;
+
+namespace SWPPT3.Main.Manager
+{
+    /// <summary>
+    ///     This controls overall lifecycle of application, and loading of scenes.
+    /// </summary>
+    public class ApplicationManager : MonoSingleton<ApplicationManager>
+    {
+
+    }
+}
