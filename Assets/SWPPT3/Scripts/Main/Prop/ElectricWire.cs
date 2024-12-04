e620af26f72f6f802b7ebf0924c48c26b0dd3af1
jihu0331 <70699703+jihu0331@users.noreply.github.com>
2024-12-04 19:25:41 +0900

    chore: allocate script to prefab, Electricwire change

--- Assets/SWPPT3/Scripts/Main/Prop/ElectricWire.cs
+++ Assets/SWPPT3/Scripts/Main/Prop/ElectricWire.cs
@@ -1,24 +1,7 @@
-using UnityEngine;
-
 namespace SWPPT3.Main.Prop
 {
-    public class ElectricWire : StateDst
+    public class ElectricWire : StateSource
     {
-        [SerializeField] private Animator animator;
 
-        protected override void OnSourceStateChanged(StateSource src, bool state)
-        {
-            if (animator != null)
-            {
-                if (this.State == On)
-                {
-                    animator.SetBool("IsOn", true);
-                }
-                else if (this.State == Off)
-                {
-                    animator.SetBool("IsOn", false);
-                }
-            }
-        }
     }
 }
