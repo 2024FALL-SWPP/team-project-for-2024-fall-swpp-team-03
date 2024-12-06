84a830bc7dd78c5d37814522f29ae02ff0f8bd22
yeonjae <joide1120@naver.com>
2024-12-06 17:39:49 +0900

    fix: change Spring physics

--- Assets/SWPPT3/Scripts/SoftbodyPhysics/Softbody.cs
+++ Assets/SWPPT3/Scripts/SoftbodyPhysics/Softbody.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Burst;
@@ -76,8 +77,10 @@ namespace SWPPT3.SoftbodyPhysics
                 };
             }
         }
+        [SerializeField]
+        private int _boneCount;
 
-        public int BoneCount { get; private set; }
+        public int BoneCount { get => _boneCount; set => _boneCount = value; }
 
         [SerializeField] private BoneWeightInfo _b0;
         [SerializeField] private BoneWeightInfo _b1;
@@ -90,7 +93,7 @@ namespace SWPPT3.SoftbodyPhysics
 
         public VertexWeightInfo(int boneCount)
         {
-            BoneCount = boneCount;
+            _boneCount = boneCount;
 
             _b0 = default;
             _b1 = default;
@@ -125,6 +128,19 @@ namespace SWPPT3.SoftbodyPhysics
         }
     }
 
+    [Serializable]
+    internal struct Bone
+    {
+        [SerializeField]  private Vector3 _position;
+        [SerializeField] private Vector3 _velocity;
+        [SerializeField] private float _mass;
+
+        public Vector3 Position{get => _position; set => _position = value;}
+        public Vector3 Velocity { get => _velocity; set => _velocity = value; }
+        public float Mass { get => _mass; set => _mass = value; }
+
+    }
+
     public class Softbody : MonoBehaviour
          {
              internal const float SphereRadius = 0.5f;
@@ -142,7 +158,10 @@ namespace SWPPT3.SoftbodyPhysics
              private float _colliderRadius = 0.05f;
 
              [HideInInspector, SerializeField]
-             private Collider[] _colliders;
+             private Collider[] _outerColliders;
+
+             [HideInInspector, SerializeField]
+             private Bone[] _bones;
 
              [HideInInspector, SerializeField]
              private List<VertexWeightInfo> _vertexWeights;
@@ -153,21 +172,22 @@ namespace SWPPT3.SoftbodyPhysics
              [HideInInspector, SerializeField]
              private List<int> _springList;
 
+             [HideInInspector, SerializeField]
+             private int _outerIndexStart;
+
              private NativeArray<VertexWeightInfo> _vertexWeightsNa;
              private NativeArray<Vector3> _vertices;
-             private NativeArray<Vector3> _bonePositionsReadOnly;
-             private NativeArray<Vector3> _bonePositionsWrite;
+
+             private NativeArray<Bone> _bonesBuffer1;
+             private NativeArray<Bone> _bonesBuffer2;
+
              private NativeArray<Spring> _springsNa;
              private NativeArray<int> _springListNa;
 
              private TransformAccessArray _colliderTransforms;
 
-
-
-             private NativeArray<Vector3> _boneVelocities;
-
-             [SerializeField] private float _springStiffness = 1f;
-             [SerializeField] private float _damping = 0.5f;
+             [SerializeField] private float _springStiffness = 10f;
+             [SerializeField] private float _damping = 2f;
 
              [SerializeField] private int _pointDensityMultiplier = 5;
              [SerializeField] private int _totalLayers = 5;
@@ -178,12 +198,12 @@ namespace SWPPT3.SoftbodyPhysics
 
              private float _elasticCoefficient;
 
+             private bool _useBuffer1 = true;
              private void Start()
              {
                  Debug.Log(_springs[0].Bone1);
-                 _colliderTransforms = new TransformAccessArray(_colliders.Length);
-                 _boneVelocities = new NativeArray<Vector3>(_colliders.Length, Allocator.Persistent);
-                 foreach (var collider in _colliders)
+                 _colliderTransforms = new TransformAccessArray(_outerColliders.Length);
+                 foreach (var collider in _outerColliders)
                  {
                      _colliderTransforms.Add(collider.transform);
                  }
@@ -192,109 +212,108 @@ namespace SWPPT3.SoftbodyPhysics
 
                  _vertices = new NativeArray<Vector3>(list.ToArray(), Allocator.Persistent);
                  _vertexWeightsNa = new NativeArray<VertexWeightInfo>(_vertexWeights.ToArray(), Allocator.Persistent);
-                 _bonePositionsReadOnly = new NativeArray<Vector3>(_colliders.Length, Allocator.Persistent);
-                 _bonePositionsWrite = new NativeArray<Vector3>(_colliders.Length, Allocator.Persistent);
+
+                 _bonesBuffer1 = new NativeArray<Bone>(_bones.ToArray(), Allocator.Persistent);
+                 _bonesBuffer2 = new NativeArray<Bone>(_bones.ToArray(), Allocator.Persistent);
+
                  _springsNa = new NativeArray<Spring>(_springs.ToArray(), Allocator.Persistent);
                  _springListNa = new NativeArray<int>(_springList.ToArray(), Allocator.Persistent);
                  Debug.Log(_springList.Count);
              }
 
+
              private void FixedUpdate()
              {
-                 var boneUpdateJob = new BonePositionsUpdateJob
-                 {
-                     BonePositionsReadOnly = _bonePositionsReadOnly
-                 };
-
-                 var boneUpdateHandle = boneUpdateJob.Schedule(_colliderTransforms);
-                 boneUpdateHandle.Complete();
+                 var _boneRead = _useBuffer1 ? _bonesBuffer1 : _bonesBuffer2;
+                 var _boneWrite = _useBuffer1 ? _bonesBuffer2 : _bonesBuffer1;
 
                  var boneSpringJob = new BoneSpringJob
                  {
-                     BonePositionsReadOnly = _bonePositionsReadOnly,
-                     BonePositionsWrite = _bonePositionsWrite,
-                     BoneVelocities = _boneVelocities,
+                     BoneRead = _boneRead,
+                     BoneWrite = _boneWrite,
                      SpringInfos = _springsNa,
                      SpringList = _springListNa,
                      DeltaTime = Time.deltaTime,
                      SpringStiffness = _springStiffness,
-                     Damping = _damping
+                     Damping = _damping,
+                     BoneCount = _bones.Length,
                  };
-                 var boneSpringHandle = boneSpringJob.Schedule(_bonePositionsReadOnly.Length, 16, boneUpdateHandle);
+                 var boneSpringHandle = boneSpringJob.Schedule(_boneRead.Length, 16);
                  boneSpringHandle.Complete();
 
                  var applyBonePositionsJob = new ApplyBonePositionsJob
                  {
-                     BonePositionsReadOnly = _bonePositionsReadOnly,
-                     BonePositionsWrite = _bonePositionsWrite
+                     BoneRead = _boneWrite,
+                     BoneWrite = _boneRead,
+                     outerIndexStart = _outerIndexStart
 
                  };
                  var applyBonePositionsHandle = applyBonePositionsJob.Schedule(_colliderTransforms, boneSpringHandle);
                  applyBonePositionsHandle.Complete();
+                 _useBuffer1 = !_useBuffer1;
+             }
 
-
+             private void Update()
+             {
                  var vertexUpdateJob = new VertexUpdateJob
                  {
                      Vertices = _vertices,
                      VertexWeights = _vertexWeightsNa,
-                     BonePositions = _bonePositionsReadOnly
+                     BoneRead = _useBuffer1 ? _bonesBuffer1 : _bonesBuffer2
                  };
 
-                 var vertexUpdateHandle = vertexUpdateJob.Schedule(_vertices.Length, 16, applyBonePositionsHandle);
+                 var vertexUpdateHandle = vertexUpdateJob.Schedule(_vertices.Length, 16);
                  vertexUpdateHandle.Complete();
 
                  _meshFilter.mesh.SetVertices(_vertices);
              }
 
-
-
              private void OnDestroy()
              {
                  if (_vertices.IsCreated) _vertices.Dispose();
                  if (_vertexWeightsNa.IsCreated) _vertexWeightsNa.Dispose();
-                 if (_bonePositionsReadOnly.IsCreated) _bonePositionsReadOnly.Dispose();
-                 if (_bonePositionsWrite.IsCreated) _bonePositionsWrite.Dispose();
-                 if (_boneVelocities.IsCreated) _boneVelocities.Dispose();
+                 if (_bonesBuffer1.IsCreated) _bonesBuffer1.Dispose();
+                 if (_bonesBuffer2.IsCreated) _bonesBuffer2.Dispose();
                  if (_springsNa.IsCreated) _springsNa.Dispose();
                  if (_springListNa.IsCreated) _springListNa.Dispose();
                  _colliderTransforms.Dispose();
              }
 
-             [BurstCompile]
-             private struct BonePositionsUpdateJob : IJobParallelForTransform
-             {
-                 public NativeArray<Vector3> BonePositionsReadOnly;
-
-                 public void Execute(int index, TransformAccess transform)
-                 {
-                     BonePositionsReadOnly[index] = transform.localPosition;
-                 }
-             }
+             // [BurstCompile]
+             // private struct BonePositionsUpdateJob : IJobParallelFor
+             // {
+             //     [ReadOnly] public NativeArray<Bone> BoneRead;
+             //     public NativeArray<Bone> BoneWrite;
+             //
+             //     public void Execute(int index)
+             //     {
+             //         BoneWrite[index] = BoneRead[index];
+             //     }
+             // }
 
              [BurstCompile]
              private struct BoneSpringJob : IJobParallelFor
              {
 
-                 public NativeArray<Vector3> BonePositionsWrite;
-                 public NativeArray<Vector3> BoneVelocities;
+                 public NativeArray<Bone> BoneWrite;
                  [ReadOnly] public NativeArray<Spring> SpringInfos;
                  [ReadOnly] public NativeArray<int> SpringList;
-                 [ReadOnly] public NativeArray<Vector3> BonePositionsReadOnly;
+                 [ReadOnly] public NativeArray<Bone> BoneRead;
 
                  public float DeltaTime;
                  public float SpringStiffness;
                  public float Damping;
 
-                 public int ColliderLength;
+                 public int BoneCount;
 
                  public void Execute(int index)
                  {
-                     Vector3 position = BonePositionsReadOnly[index];
-                     Vector3 velocity = BoneVelocities[index];
+                     Vector3 position = BoneRead[index].Position;
+                     Vector3 velocity = BoneRead[index].Velocity;
                      Vector3 force = Vector3.zero;
 
-                     var start = index * ColliderLength;
-                     var end = start + ColliderLength;
+                     var start = index * BoneCount;
+                     var end = start + BoneCount;
 
                      for (int i = start; i < end; i++)
                      {
@@ -306,8 +325,8 @@ namespace SWPPT3.SoftbodyPhysics
                              var bone2  = spring.Bone2;
                              var restLength = spring.RestLength;
 
-                             Vector3 pos1 = BonePositionsReadOnly[bone1];
-                             Vector3 pos2 = BonePositionsReadOnly[bone2];
+                             Vector3 pos1 = BoneRead[bone1].Position;
+                             Vector3 pos2 = BoneRead[bone2].Position;
 
                              Vector3 springVector = pos2 - pos1;
                              float currentLength = springVector.magnitude;
@@ -329,21 +348,29 @@ namespace SWPPT3.SoftbodyPhysics
 
                      position += velocity * DeltaTime;
 
-                     BoneVelocities[index] = velocity;
-                     BonePositionsWrite[index] = position;
+                     Debug.Log($"{index} {velocity} {force},");
+
+                     BoneWrite[index] = new Bone
+                     {
+                         Position = position,
+                         Velocity = velocity,
+                         Mass = BoneRead[index].Mass,
+                     };
                  }
              }
 
              [BurstCompile]
              private struct ApplyBonePositionsJob : IJobParallelForTransform
              {
-                 [ReadOnly] public NativeArray<Vector3> BonePositionsWrite;
-                 public NativeArray<Vector3> BonePositionsReadOnly;
+                 [ReadOnly] public NativeArray<Bone> BoneRead;
+                 [ReadOnly] public int outerIndexStart;
+                 public NativeArray<Bone> BoneWrite;
 
                  public void Execute(int index, TransformAccess transform)
                  {
-                     transform.localPosition = BonePositionsWrite[index];
-                     BonePositionsReadOnly[index] = transform.position;
+                     BoneWrite[index] = BoneRead[index];
+                     if(index >= outerIndexStart)
+                         transform.localPosition = BoneRead[index - outerIndexStart].Position;
                  }
              }
 
@@ -352,7 +379,7 @@ namespace SWPPT3.SoftbodyPhysics
              {
                  public NativeArray<Vector3> Vertices;
                  [ReadOnly] public NativeArray<VertexWeightInfo> VertexWeights;
-                 [ReadOnly] public NativeArray<Vector3> BonePositions;
+                 [ReadOnly] public NativeArray<Bone> BoneRead;
 
                  public void Execute(int index)
                  {
@@ -362,7 +389,7 @@ namespace SWPPT3.SoftbodyPhysics
                      for (var j = 0; j < boneInfo.BoneCount; j++)
                      {
                          var weightInfo = boneInfo[j];
-                         var bonePosition = BonePositions[weightInfo.BoneIndex];
+                         var bonePosition = BoneRead[weightInfo.BoneIndex].Position;
                          var boneDesiredVertex = bonePosition + weightInfo.Offset;
                          v += boneDesiredVertex * weightInfo.Weight;
                      }
