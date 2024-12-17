9d5ef7f66e0d6926008de07006c80086315d6d43
jihu0331 <70699703+jihu0331@users.noreply.github.com>
2024-12-17 16:56:22 +0900

    UIManager

--- Assets/SWPPT3/Scripts/Main/PlayerLogic/State/PlayerState.cs
+++ Assets/SWPPT3/Scripts/Main/PlayerLogic/State/PlayerState.cs
@@ -25,7 +25,7 @@ namespace SWPPT3.Main.PlayerLogic.State
             else if (obstacle is PoisonPool)
             {
                 //Debug.Log("collide with Poison pool");
-                GameManager.Instance.OnPlayerStateChanged("GameOver");
+                GameManager.Instance.GameState = GameState.GameOver;
             }
             else if (obstacle is Gas)
             {
@@ -37,7 +37,7 @@ namespace SWPPT3.Main.PlayerLogic.State
             }
             else if (obstacle is MagicCircle)
             {
-                GameManager.Instance.OnPlayerStateChanged("StageCleared");
+                GameManager.Instance.GameState = Gamestate.StageCleared;
             }
             else
             {
